4fbe852063c0d8d68b9842b6e41bb6d4e09d5756
Dohun9196 <68097610+dohun9196@users.noreply.github.com>
2022-11-28 15:44:39 +0900

    Add files via upload

--- js/banner01.js
+++ js/banner01.js
@@ -1,17 +1,19 @@
 $(function () {
+
     $('.topBanner i').on('click', function () {
         $('.topBanner').toggleClass('on')
     });
+
 
     $('.topBanner2 i').on('click', function () {
         $('.topBanner2 .inner').slideToggle();
     });
 
     $('.bottomBanner i').on('click', function () {
-        $('.bottomBanner .inner').slideToggle('on');
+        $('.bottomBanner .inner').slideToggle()
     });
 
     $('.popup button').on('click', function () {
-        $('this').parent().hide()
-    });
+        $(this).parent().hide();
+    })
 })
