bdb709a8d1bc0e9cbfd2f9b687a6e1dd0ed483d0
Dohun9196 <68097610+dohun9196@users.noreply.github.com>
2022-12-01 16:11:26 +0900

    Add files via upload

--- js/graff.js
+++ js/graff.js
@@ -41,4 +41,22 @@ $(function () {
 
 
     //tab menu
+
+    $('.mainCustomer .tab_menu>li').on('click', function (event) {
+        event.preventDefault();
+        var _this = $(this);            // 이벤트가 적용된 자신을 반환
+        var idx = $(this).index();      // 이벤트가 적용된 자신의 번호를 반환 0,1,2
+        _this.addClass('on').siblings().removeClass('on');
+        $('.mainCustomer .tab_content>li').eq(idx).addClass('on')
+            .siblings().removeClass('on')
+    });
+
+    $('#f_link').on('change', function () {
+        console.log('change', $(this).val());
+        var link = $(this).val();
+
+        if (link) {
+            window.open(link)
+        }
+    })
 })
